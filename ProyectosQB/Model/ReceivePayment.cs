92ffefcb745e39b0361f575e8270920cf6ca1f03
jcfigueroaferia <j.figueroaferia@icloud.com>
2015-08-07 18:00:54 -0500

    Se agregaron los datos para la seleccion de la clase payment

--- ProyectosQB/Model/ReceivePayment.cs
+++ ProyectosQB/Model/ReceivePayment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,23 @@ namespace Model
 {
     public class ReceivePayment
     {
-        public ReceivePayment() { }
+        /*Nota: Aqui se agregara los datos a pedir de customer Payment*/
+
+        private string refNumber;
+        private string deposit;
+        private double amount;
+        private string memo;
+        public ReceivePayment()
+        {
+            refNumber = "";
+            deposit = "";
+            amount = 0d;
+            memo = "";  
+        }
+        public string  RefNumber { get; set; }
+        public string Deposit { get; set; }
+        public double Amount { get; set; }
+        public string Memo { get; set; }
+        public string date { get; set; }
     }
 }
