48553d2e6b7a58ff650cdf76fc60aeb379b76d1f
jcfigueroaferia <j.figueroaferia@icloud.com>
2015-08-07 17:02:15 -0500

    Creacion de los atributos que conforman los billpayment

--- ProyectosQB/Model/BillPayment.cs
+++ ProyectosQB/Model/BillPayment.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace Model
 {
-    public class Class1
+    public class BillPayment
     {
+        private string refNumber;
+        private string memo;
+        public BillPayment()
+        {
+            refNumber = "";
+            memo = "";
+        }
+
+        public string RefNumber
+        {
+            get { return refNumber; }
+            set { refNumber = value; }
+        }
+        public string Memo
+        {
+            get { return memo; }
+            set { memo = value; }
+        }
     }
 }
