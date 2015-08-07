c1745ff5712273f35703e34d4cf610dbcc0c97ff
jcfigueroaferia <j.figueroaferia@icloud.com>
2015-08-07 17:35:01 -0500

    Se creo la clase invoice

---
+++ ProyectosQB/Model/Invoice.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Invoice
+    {
+        /*Nota: Attibutos de la clase invoice y la interfaz invoice*/
+        private double tax;
+        private string numInvoice;
+        private string address;
+        private string memo;
+        public Invoice()
+        {
+            tax = 0f;
+            numInvoice = "";
+            address = "";
+            memo = "";
+        }
+
+        public double Tax
+        {
+            get { return tax; }
+            set { tax = value; }
+        }
+
+        public string NumInvoice
+        {
+            get { return numInvoice; }
+            set { numInvoice = value; }
+        }
+
+        public string Address
+        {
+            get { return address; ; }
+            set { address = value; }
+        }
+
+        public string Memo
+        {
+            get { return memo; }
+            set { memo = value; }
+        }
+    }
+}
