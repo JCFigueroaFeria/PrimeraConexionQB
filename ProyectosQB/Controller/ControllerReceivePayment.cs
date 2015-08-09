3932c9fe8e77a186796c1bb7218a54e87c3c3d65
jcfigueroaferia <j.figueroaferia@icloud.com>
2015-08-08 22:50:55 -0500

    se agrego los parametro para incializar la carga de valores en el tabla

--- ProyectosQB/Controller/ControllerReceivePayment.cs
+++ ProyectosQB/Controller/ControllerReceivePayment.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataAccess;
+using Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,15 @@ namespace Controller
 {
     public class ControllerReceivePayment
     {
-        public ControllerReceivePayment() { }
+
+        private DAReceivePayment daReceive;
+        public ControllerReceivePayment() {
+            daReceive = new DAReceivePayment();
+        }
+
+        public List<ReceivePayment> cargarReceivePayment(DateTime fechaInicio, DateTime fechaTermino)
+        {
+            return daReceive.getListReceivePayment(fechaInicio, fechaTermino);
+        }
     }
 }
