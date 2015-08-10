7f55b892ef0549852e98da48fe52004a8e8615ff
jcfigueroaferia <j.figueroaferia@icloud.com>
2015-08-10 12:22:41 -0500

    Clases que faltanab por exportar

---
+++ ProyectosQB/Business/Discount.cs
@@ -0,0 +1,25 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+
+    public class Discount
+    {
+        private float descuento;
+
+        public Discount()
+        {
+
+            descuento = .17f;
+        }
+        public void aplicarDecuento(Bill billDiscount)
+        {
+            billDiscount.Discount = (float)(billDiscount.Amount * descuento);
+        }
+    }
+}
---
+++ ProyectosQB/ProyectosQB/VentanaBill.xaml.cs
@@ -0,0 +1,70 @@
+﻿using Business;
+using Controller;
+using MahApps.Metro.Controls;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectosQB
+{
+    /// <summary>
+    /// Lógica de interacción para VentanaBill.xaml
+    /// </summary>
+    public partial class VentanaBill : MetroWindow
+    {
+        ControllerBill cargarBill;
+        Discount discount;
+        public VentanaBill()
+        {
+            InitializeComponent();
+            cargarBill = new ControllerBill();
+            discount = new Discount();
+            dpFechaInicio.SelectedDate = dpFechaTermino.SelectedDate = DateTime.Now;
+        }
+
+        private void btntableload_Click(object sender, RoutedEventArgs e)
+        {
+            cargarTablaPorFechas();
+        }
+
+        private void btnBuscarReferent_Click(object sender, RoutedEventArgs e)
+        {
+            cargarTablaPorReferencia();
+        }
+
+        public void cargarTablaPorFechas()
+        {
+
+            List<Bill> bills = cargarBill.cargarDatosBill((DateTime)dpFechaInicio.SelectedDate, (DateTime)dpFechaTermino.SelectedDate);
+            aplicarDescuento(bills);
+        }
+
+
+        public void cargarTablaPorReferencia()
+        {
+            List<Bill> listBill = new List<Bill>();
+            Bill rBill = cargarBill.cargarBillReferent(txtRefNumber.Text);
+            listBill.Add(rBill);
+            aplicarDescuento(listBill);
+        }
+        private void aplicarDescuento(List<Bill> bills)
+        {
+            foreach (var bill in bills)
+            {
+                discount.aplicarDecuento(bill);
+            }
+            this.tblReceive.ItemsSource = bills;
+        }
+    }
+}
