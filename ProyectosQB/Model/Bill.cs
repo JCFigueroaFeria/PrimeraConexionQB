fd1ffe4bb5e5470b8e8ba10b8f5ce4b60d3c287b
jcfigueroaferia <j.figueroaferia@icloud.com>
2015-08-10 09:45:14 -0500

    Se agrego el modelo Bill al proyecto

---
+++ ProyectosQB/Model/Bill.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Bill
+    {
+
+       private string vendor;
+       private DateTime date;
+       private string refnumber;
+       private double amount;
+       private float discount;
+        public Bill() {
+
+            vendor = "";
+            refnumber = "";
+            amount = 0d;
+            discount = 0f;
+
+        }
+
+
+        public string Vendor { get { return vendor; } set { vendor = value; } }
+        public DateTime Date { get { return date; } set { date = value; } }
+        public string RefNumber { get { return refnumber; } set { refnumber = value; } }
+        public double Amount { get { return amount; } set { amount = value; } }
+
+        public float Discount { get { return discount; } set { discount = value; } }
+    }
+}
