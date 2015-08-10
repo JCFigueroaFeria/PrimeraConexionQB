9bf11101114f07c98ab8e36be4f0bc49e6ce2f21
jcfigueroaferia <j.figueroaferia@icloud.com>
2015-08-10 10:26:10 -0500

    Actualizacion del proyecto

---
+++ ProyectosQB/Controller/ControllerBill.cs
@@ -0,0 +1,23 @@
+﻿using DataAccess;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller
+{
+    public class ControllerBill
+    {
+        private DABill daBills;
+        public ControllerBill() {
+            daBills = new DABill();
+        }
+
+        public List<Bill> cargarDatosBill(DateTime fechaInicio, DateTime fechaTermino)
+        {
+            return daBills.getListBillDate(fechaInicio, fechaTermino);
+        }
+    }
+}
